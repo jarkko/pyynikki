aa89981328bcebd83a70ec8a17d096692b430ac1
punakone <punakone@e5e2f212-7e41-0410-90ad-8622a03ce8cc>
2008-02-18 17:37:35 +0000

    added l10n-simplified
    git-svn-id: http://www.punakone.com/svn/pyynikki/trunk@10 e5e2f212-7e41-0410-90ad-8622a03ce8cc

---
+++ vendor/plugins/l10n-simplified/init.rb
@@ -0,0 +1,16 @@
+# set L10N_LANG constant in your environment.rb or here
+# 
+# Choose language file here. Only one language file can be selected
+#L10N_LANG ||= :da
+
+#require 'lang_en' #default language to run the test cases
+require  "lang_" + L10N_LANG.to_s
+
+#require 'lang_de', 'lang_es', 'lang_fi', 'lang_fr', 'lang_nl', 'lang_ko', 'lang_se', 'lang_chef', 'lang_pirate'
+# You can Add your own localization file and add it here
+
+
+
+# Also include hook code here
+require 'localization_simplified'
+
