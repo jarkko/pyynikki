27cb6778b75c4946ee1ce7792a9c0b06092fee48
punakone <punakone@e5e2f212-7e41-0410-90ad-8622a03ce8cc>
2008-02-18 18:43:48 +0000

    modified config/deploy to use with cap
    git-svn-id: http://www.punakone.com/svn/pyynikki/trunk@14 e5e2f212-7e41-0410-90ad-8622a03ce8cc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "pyynikki"
+set :repository,  "http://www.punakone.com/svn/pyynikki/trunk"
+set :rails_dir, "/home/jarkko/projects/rails"
+
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -9,7 +11,33 @@ set :repository,  "set your repository location here"
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+ssh_options[:port] = 6969
+set :deploy_to, "/home/jarkko/sites/pyynikki"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "jlaine.net"
+role :web, "jlaine.net"
+role :db,  "jlaine.net", :primary => true
+
+desc "Checks out rails rev ##{rails_version}"
+task :after_symlink do
+  run <<-CMD
+    svn up -r #{rails_version} #{rails_dir} &&
+    cd #{current_release} &&
+    ln -s #{rails_dir} vendor/rails
+  CMD
+end
+
+desc "Push the latest svk code to the svn repo and set the svn revision number"
+task :before_update_code do
+  system("rake git:push")
+end
+
+namespace :deploy do
+  desc "Restart mongrel cluster"
+  task :restart do
+    run <<-CMD
+      cd #{current_release} &&
+      mongrel_rails cluster::restart
+    CMD
+  end
+end
