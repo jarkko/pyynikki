d100ba2c806120dacec37243fee9187d58b5987a
Jarkko Laine <jarkko@jlaine.net>
2010-01-08 17:23:08 +0200

    config.gem "xml-simple"

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "shoulda"
   config.gem "formtastic"
+  config.gem "xml-simple"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
